1460997c8844a4558c4f2f0504c40332ab8b7c83
Alexandr <shatokhin.04@inbox.ru>
2020-07-02 00:44:01 +0300

    Add js + slick-complete

---
+++ src/js/script.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+    $('.carousel__inner').slick({
+        autoplay: true,
+        prevArrow: '<button type="button" class="slick-prev"><img src="icons/left.png" alt="left"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="icons/right.png" alt="right"></button>',
+        adaptiveHeight: true
+    });
+  });
